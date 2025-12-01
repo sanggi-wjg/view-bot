cb25a88b65e28e4c64448b751e88e92e820d86cc
Raynor <jonggu.woo@fitpet.co.kr>
2025-12-01 12:11:47 +0900

    Fix import paths in `view_bot` and update `__all__` for `bots` module

--- view_bot/bots/__init__.py
+++ view_bot/bots/__init__.py
@@ -1,3 +1,4 @@
-from viewerbot.bots.custom_viewbot import CustomViewBot
+from view_bot.bots.custom_viewbot import CustomViewBot
+from view_bot.bots.viewbot import ViewBot
 
-__all__ = ["CustomViewBot"]
+__all__ = ["ViewBot", "CustomViewBot"]
